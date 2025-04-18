ba2d5d649b0a8b73b86ce3c0725021ba9ecb934a
Daniele Baccega <94167431+daniele-baccega@users.noreply.github.com>
2025-04-18 14:46:28 +0200

    Update generate_xml.py

--- resources/generate_xml.py
+++ resources/generate_xml.py
@@ -558,12 +558,12 @@ def generate_xml(input_file, random_seed, rooms, areas, pedestrian_names, agents
 							env_events_cdf[agent_type_idx][e] = float(rf["Weight"])
 						else:
 							env_events_cdf[agent_type_idx][e] = float(rf["Weight"]) + env_events_cdf[agent_type_idx][e+1]
-					env_events_activity[agent_type_idx][e] = rf["Activity"]
-					env_events_distr[agent_type_idx][e] = distributions[rf["Dist"]]
-					env_events_distr_firstparam[agent_type_idx][e] = int(a) * steps_in_a_minute
-					env_events_distr_secondparam[agent_type_idx][e] = int(b) * steps_in_a_minute
-
-					e = e - 1
+						env_events_activity[agent_type_idx][e] = rf["Activity"]
+						env_events_distr[agent_type_idx][e] = distributions[rf["Dist"]]
+						env_events_distr_firstparam[agent_type_idx][e] = int(a) * steps_in_a_minute
+						env_events_distr_secondparam[agent_type_idx][e] = int(b) * steps_in_a_minute
+	
+						e = e - 1
 					
 					env_events[agent_type_idx][0] = 0
 					env_events_area[agent_type_idx][0] = -1
