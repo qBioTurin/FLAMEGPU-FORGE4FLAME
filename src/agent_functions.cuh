f93731d45dc2e70f0b68fcd46e606bdd9e4aa61f
Daniele Baccega <daniele.baccega@unito.it>
2025-05-09 12:22:37 +0200

    Fixed a bug?

--- src/agent_functions.cuh
+++ src/agent_functions.cuh
@@ -216,10 +216,10 @@ FLAMEGPU_AGENT_FUNCTION(CUDAInitContagionScreeningEventsAndMovePedestrian, Messa
         env_events_mapping[num_events] = 0;
         for(int j = num_events; j > 0; j--){
             if(j == num_events)
-                env_events_cdf[j-1] = (float) env_events_probability[env_events_mapping[j]];
+                env_events_cdf[j-1] = (float) env_events_probability[agent_type][env_events_mapping[j]];
             else
-                env_events_cdf[j-1] = env_events_cdf[j] + (float) env_events_probability[env_events_mapping[j]];
-            printf("6,%d,id: %d step: %d cdf: %f prob: %f\n", FLAMEGPU->environment.getProperty<unsigned short>(RUN_IDX), contacts_id, step, env_events_cdf[j-1], (float) env_events_probability[env_events_mapping[j]]);
+                env_events_cdf[j-1] = env_events_cdf[j] + (float) env_events_probability[agent_type][env_events_mapping[j]];
+            printf("6,%d,id: %d step: %d cdf: %f prob: %f\n", FLAMEGPU->environment.getProperty<unsigned short>(RUN_IDX), contacts_id, step, env_events_cdf[j-1], (float) env_events_probability[agent_type][env_events_mapping[j]]);
         }
 
         unsigned short event = env_events_mapping[findLeftmostIndex(FLAMEGPU, random, env_events_cdf, num_events)];
