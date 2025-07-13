d522e3d2e27dabfe6c796a2ad7853de444c705c3
Daniele Baccega <daniele.baccega@unito.it>
2025-07-13 15:40:28 +0200

    Updated ylim for alert

--- reproduce_alert.sh
+++ reproduce_alert.sh
@@ -23,5 +23,13 @@ python barplot.py -experiment_dirs AlarmNoCountermeasures AlarmSurgical20 AlarmS
                   -experiment_labels Baseline Surgical-20% Surgical-40% Surgical-80% FFP2-20% FFP2-40% FFP2-80% \
                   -day_x 30 \
                   -baseline_experiment AlarmNoCountermeasures
+python barplot.py -experiment_dirs AlarmNoCountermeasures AlarmSurgical20 AlarmSurgical40 AlarmSurgical80 AlarmFFP220 AlarmFFP240 AlarmFFP280 \
+                  -experiment_labels Baseline Surgical-20% Surgical-40% Surgical-80% FFP2-20% FFP2-40% FFP2-80% \
+                  -day_x 60 \
+                  -baseline_experiment AlarmNoCountermeasures
+python barplot.py -experiment_dirs AlarmNoCountermeasures AlarmSurgical20 AlarmSurgical40 AlarmSurgical80 AlarmFFP220 AlarmFFP240 AlarmFFP280 \
+                  -experiment_labels Baseline Surgical-20% Surgical-40% Surgical-80% FFP2-20% FFP2-40% FFP2-80% \
+                  -day_x 90 \
+                  -baseline_experiment AlarmNoCountermeasures
 cd ..
 deactivate
